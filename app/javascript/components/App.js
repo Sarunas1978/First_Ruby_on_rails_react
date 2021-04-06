3c56aa1b52e1494313c217096d13915b9f6ea7ec
Saras Maras <sarunas.mailas@gmail.com>
2021-04-06 17:12:04 +0300

    bootstrap added

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default class App extends React.Component {
     render(){
         return(
-            <>
+            <div className="container">
             <div>Rails</div>
             <div>Rails Rails</div>
-            <div>Rails Rails Rails</div>
-            </>
+            <div className="text-danger">Rails Rails Rails</div>
+            </div>
         )
     }
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,10 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+require('Turbolinks').start()
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap'
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
